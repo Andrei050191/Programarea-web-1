33a156c00baad36367874febf98ad7933b930f34
Andrei <ungureanuandrei163303@gmail.com>
2026-01-17 13:02:00 +0200

    Function , task empty

---
+++ JS/Exercises/Functions/Task 1 Function Declaration/app.js
@@ -0,0 +1,10 @@
+// function declaration
+function sum(a, b) {
+  return a + b;
+}
+
+// apelarea funcției
+let result = sum(10, 20);
+
+// afișare rezultat
+console.log("Suma numerelor este:", result);
---
+++ JS/Exercises/Functions/Task 2 Function Expression/app.js
@@ -0,0 +1,10 @@
+// function expression
+const square = function (number) {
+  return number * number;
+};
+
+// apelarea funcției
+let result = square(5);
+
+// afișare rezultat
+console.log("Pătratul numărului este:", result);
